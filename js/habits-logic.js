42f271733bcf94e5b0b267235b1c51a19b9e3c21
Barac Sebastian-Beniamin <barac.sebastian.beniamin@gmail.com>
2025-07-30 13:11:39 +0300

    refactor: optimize entry rendering logic in renderHabit function

--- js/habits-logic.js
+++ js/habits-logic.js
@@ -31,21 +31,18 @@ function renderHabit(habit) {
     const entriesContainer = document.createElement("div");
     entriesContainer.className = "entry-container";
 
+    const entryMap = {};
+    for (const entry of habit.entries) {
+        entryMap[entry.date] = entry.value;
+    }
+
     for (let index = 0; index < 7; index++) {
         // we have to display only 7 values
         const entryDiv = document.createElement("div");
         entryDiv.className = "entry";
 
-        let value = "?";
-        for (const entry of habit.entries) {
-            // here we are searching for the entry object within the habit.entries array which has the date property value
-            // the same as the date above the entry that we want to change (so basically making sure that we render the entry in the right place)
-            if (entry.date === currentDisplayedDates[index]) {
-                value = entry.value;
-                break;
-            }
-        }
-        entryDiv.innerHTML = value;
+        const date = currentDisplayedDates[index];
+        entryDiv.innerHTML = entryMap[date] || "?";
 
         entryDiv.addEventListener("click", () => {
             habit.entries.push(addEntry(index));
